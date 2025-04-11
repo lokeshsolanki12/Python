11f669d97aa7898f785612e780aea35a383b3365
Lokesh Solanki <lokesh.solanki7014.in>
2025-04-11 16:53:29 +0530

    Add the Commands

--- Dictionari.py
+++ Dictionari.py
@@ -1,73 +1,73 @@
-# empty_dict = {}
-# print(type(empty_dict))
+empty_dict = {}
+print(type(empty_dict))
 
-# empty_dict = dict()
-# print(empty_dict)
+empty_dict = dict()
+print(empty_dict)
 
-# student = {"name":"lokesh","age":18,"grade":"A"}
-# print(student)
+student = {"name":"lokesh","age":18,"grade":"A"}
+print(student)
 
-# student1 = {"name":"lokesh","age":18,"name":"Akashay"}
-# print(student1)  ## No repted the same key in the Dictionary
+student1 = {"name":"lokesh","age":18,"name":"Akashay"}
+print(student1)  ## No repted the same key in the Dictionary
 
 ### AccessingnDictionary Elements
-# student = {"name":"lokesh","age":18,"grade":"A"}
-# print(student['grade'])
-# print(student['name'])
+student = {"name":"lokesh","age":18,"grade":"A"}
+print(student['grade'])
+print(student['name'])
 
 ### Accessing using get() method
 
-# print(student.get('name'))
-# print(student.get('age'))
+print(student.get('name'))
+print(student.get('age'))
 
 ### Modify Dictonary Elements
 '''
 :- Dictionary are mutable, So you can add, update or delete element
 '''
 
-# student["addersh"] = "India" ## Add the new key value
-# print(student)
-# student["age"] = 19   ## Modify the value
-# print(student)
+student["addersh"] = "India" ## Add the new key value
+print(student)
+student["age"] = 19   ## Modify the value
+print(student)
 
 ### Delete key
 
-# del student["grade"]
-# print(student)
+del student["grade"]
+print(student)
 
 ### Comman Dictories Methods
 
-# keys = student.keys() ## get all keys
-# print(keys)
+keys = student.keys() ## get all keys
+print(keys)
 
-# values = student.values()  ## get all values
-# print(values)
+values = student.values()  ## get all values
+print(values)
 
-# items = student.items()  ## get all key and values
-# print(items)   
+items = student.items()  ## get all key and values
+print(items)   
 
 ### Shallow copy
 '''
 When we are normaly copy the any dictionary then this is all time the copy the same values.
 after the modify this is the proble for solving the problem we are use the shallow copy'''
 
-# student_copy = student
-# print(student)
-# print(student_copy)
+student_copy = student
+print(student)
+print(student_copy)
 
-# student["name"] = "Lokesh Solanki"
-# print(student)                     ##{'name': 'Lokesh Solanki', 'age': 19, 'addersh': 'India'}
-# print(student_copy)                ##{'name': 'Lokesh Solanki', 'age': 19, 'addersh': 'India'}
+student["name"] = "Lokesh Solanki"
+print(student)                     ##{'name': 'Lokesh Solanki', 'age': 19, 'addersh': 'India'}
+print(student_copy)                ##{'name': 'Lokesh Solanki', 'age': 19, 'addersh': 'India'}
 
 ## solving the problem we are use the shallow copy
 
-# student_copy1 = student.copy()
-# print(student_copy1)
-# print(student)
+student_copy1 = student.copy()
+print(student_copy1)
+print(student)
 
-# student["name"] = "Lokesh bharat"
-# print(student_copy1)      ##{'name': 'Lokesh Solanki', 'age': 19, 'addersh': 'India'}
-# print(student)          ##{'name': 'Lokesh bharat', 'age': 19, 'addersh': 'India'}
+student["name"] = "Lokesh bharat"
+print(student_copy1)      ##{'name': 'Lokesh Solanki', 'age': 19, 'addersh': 'India'}
+print(student)          ##{'name': 'Lokesh bharat', 'age': 19, 'addersh': 'India'}
 
 ### interating over Dictionaries 
 '''
